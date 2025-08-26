f6674f845e6182e1c51025c2aac01a1b5b2c6b3c
hesamzia <sasan.ziaee49@gmail.com>
2025-08-26 15:54:45 +0300

    Game over if the snake hits the walls.

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Experiment-implementing-games-Python
 An experiment for implementing games with Python
 I want to make a package of different games that includes old games with new changes. The first game is Snake, which I will make some changes to.
+
+# TODO
+- [X] Game over if the snake hits the walls.
+- [ ] Add public and config varable to config file.
+- [ ] Feeding the snake and its growth
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+
 pygame.init()
 
 # colors definition
@@ -6,8 +8,11 @@ white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
 
+dis_width = 800 # TODO (add to config file)width of display window
+dis_height  = 600 # height of display window
+
 # set display window of game
-dis=pygame.display.set_mode((800,600))
+dis=pygame.display.set_mode((dis_width,dis_height))
 
 # set title of game window
 pygame.display.set_caption('Snake Game')
@@ -15,13 +20,23 @@ pygame.display.set_caption('Snake Game')
 
 game_over=False # game over flag
 
-x1 = 300 # initial position of snake
-y1 = 300
+x1 = dis_width/2  # initial position of snake on x axis
+y1 = dis_height/2
  
 x1_change = 0      # initial change in position
 y1_change = 0
 
 clock = pygame.time.Clock() # to control speed of snake
+
+snake_speed=30 # TODO (add to config file) speed of snake
+
+font_style = pygame.font.SysFont(None, 50) #  font style and size
+
+
+def message(msg,color): # function to display message on screen
+    mesg = font_style.render(msg, True, color) # render the message
+    mesg_rect = mesg.get_rect(center=(dis_width/2, dis_height/2)) # get rectangle of message
+    dis.blit(mesg, mesg_rect) # display the message on screen
  
 while not game_over: # main loop of game
     for event in pygame.event.get(): # event handling loop
@@ -40,14 +55,22 @@ while not game_over: # main loop of game
         elif event.key == pygame.K_DOWN:  # if down arrow key is pressed
             y1_change = 10
             x1_change = 0            
+
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
+        
     x1 += x1_change             # update position of snake
     y1 += y1_change
     dis.fill(white)          # fill display with white color
     pygame.draw.rect(dis, black, [x1, y1, 10, 10]) # draw snake
     
-    clock.tick(30)        # control speed of snake
-
     pygame.display.update()   # update display
+
+    clock.tick(snake_speed)        # control speed of snake
+    
+message("You lost",red)
+pygame.display.update() # display message on screen for 2 seconds and then quit game 
+time.sleep(2) 
 pygame.quit()
 quit()
 
