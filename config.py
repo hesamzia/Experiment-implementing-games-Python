a6b758e5f96b2f3e244eacf3b9a58d48b4207ec2
hesamzia <sasan.ziaee49@gmail.com>
2025-08-26 19:36:34 +0300

    Add public and config varable to config file

--- config.py
+++ config.py
@@ -4,3 +4,11 @@ MESSAGECOLOR = (213, 50, 80) # red
 FOODCOLOR = (0, 255, 0) # green
 DISPLAYCOLOR = (50, 153, 213) # blue
 #white = (255, 255, 255) 
+
+DIS_WIDTH = 600 # width of display window
+DIS_HEIGHT  = 400 # height of display window
+
+SNAKE_BLOCK = 10 # size of snake block
+SNAKE_SPEED = 10 # speed of snake
+
+
--- main.py
+++ main.py
@@ -6,27 +6,14 @@ import config  # import config file
 
 pygame.init()
 
-# colors definition
-#white = (255, 255, 255) # TODO (add to config file) define colors in config file
-#yellow = (255, 255, 102)
-#black = (0, 0, 0)
-#red = (213, 50, 80)
-#green = (0, 255, 0)
-#blue = (50, 153, 213)
-
-dis_width = 600 # TODO (add to config file)width of display window
-dis_height  = 400 # height of display window
-
 # set display window of game
-dis = pygame.display.set_mode((dis_width,dis_height))
+dis = pygame.display.set_mode((config.DIS_WIDTH, config.DIS_HEIGHT))
 
 # set title of game window
 pygame.display.set_caption('Snake Game')
 
 clock = pygame.time.Clock()
 
-snake_block=10 # TODO (add to config file) size of snake block
-snake_speed=10 # TODO (add to config file) speed of snake
 
 font_style = pygame.font.SysFont("bahnschrift", 25) # another font style and size
 score_font = pygame.font.SysFont("comicsansms", 35)
@@ -39,12 +26,12 @@ def Your_score(score): # function to display score
 
 def our_snake(snake_block, snake_list): # function to display snake
     for x in snake_list:
-        pygame.draw.rect(dis, config.SNAKECOLOR, [x[0], x[1], snake_block, snake_block]) # draw rectangle for snake
+        pygame.draw.rect(dis, config.SNAKECOLOR, [x[0], x[1], snake_block, config.SNAKE_BLOCK]) # draw rectangle for snake
 
 
 def message(msg,color): # function to display message on screen
     mesg = font_style.render(msg, True, color) # render the message
-    mesg_rect = mesg.get_rect(center=(dis_width/2, dis_height/2)) # get rectangle of message
+    mesg_rect = mesg.get_rect(center=(config.DIS_WIDTH/2, config.DIS_HEIGHT/2)) # get rectangle of message
     dis.blit(mesg, mesg_rect) # display the message on screen
  
 
@@ -53,8 +40,8 @@ def gameLoop(): # main function of game
     game_over=False # game over flag
     game_close=False # game close flag
 
-    x1 = dis_width/2  # initial position of snake on x axis
-    y1 = dis_height/2
+    x1 = config.DIS_WIDTH/2  # initial position of snake on x axis
+    y1 = config.DIS_HEIGHT/2
     
     x1_change = 0      # initial change in position
     y1_change = 0
@@ -62,8 +49,8 @@ def gameLoop(): # main function of game
     snake_List = [] # list to store position of snake
     Length_of_snake = 1
 
-    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0 # initial position of food on x axis
-    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+    foodx = round(random.randrange(0, config.DIS_WIDTH - config.SNAKE_BLOCK) / 10.0) * 10.0 # initial position of food on x axis
+    foody = round(random.randrange(0, config.DIS_HEIGHT - config.SNAKE_BLOCK) / 10.0) * 10.0
  
 
     while not game_over: # main loop of game
@@ -87,26 +74,26 @@ def gameLoop(): # main function of game
                 game_over = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT: # if left arrow is pressed
-                    x1_change = -snake_block # change in position
+                    x1_change = -config.SNAKE_BLOCK # change in position
                     y1_change = 0
                 elif event.key == pygame.K_RIGHT:
-                    x1_change = snake_block
+                    x1_change = config.SNAKE_BLOCK
                     y1_change = 0
                 elif event.key == pygame.K_UP:
-                    y1_change = -snake_block
+                    y1_change = -config.SNAKE_BLOCK
                     x1_change = 0
                 elif event.key == pygame.K_DOWN:
-                    y1_change = snake_block
+                    y1_change = config.SNAKE_BLOCK
                     x1_change = 0
 
-        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0: # if snake hits the wall
+        if x1 >= config.DIS_WIDTH or x1 < 0 or y1 >= config.DIS_HEIGHT or y1 < 0: # if snake hits the wall
             game_close = True
             
         x1 += x1_change             # update position of snake
         y1 += y1_change
         dis.fill(config.DISPLAYCOLOR) # fill display with DISPLAYCOLOR color
 
-        pygame.draw.rect(dis, config.FOODCOLOR, [foodx, foody, snake_block, snake_block]) # draw food on screen
+        pygame.draw.rect(dis, config.FOODCOLOR, [foodx, foody, config.SNAKE_BLOCK, config.SNAKE_BLOCK]) # draw food on screen
         snake_Head = [] # list to store position of snake head
         snake_Head.append(x1) # append x and y position of snake head
         snake_Head.append(y1)  
@@ -118,17 +105,17 @@ def gameLoop(): # main function of game
             if x == snake_Head:
                 game_close = True
 
-        our_snake(snake_block, snake_List)        # call function to display snake 
+        our_snake(config.SNAKE_BLOCK, snake_List)        # call function to display snake 
         Your_score(Length_of_snake - 1)           # call function to display score
 
         pygame.display.update()
 
         if x1 == foodx and y1 == foody: # if snake eats the food
-            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0 # initial position of food on x axis
-            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            foodx = round(random.randrange(0, config.DIS_WIDTH - config.SNAKE_BLOCK) / 10.0) * 10.0 # initial position of food on x axis
+            foody = round(random.randrange(0, config.DIS_HEIGHT - config.SNAKE_BLOCK) / 10.0) * 10.0
             Length_of_snake += 1
 
-        clock.tick(snake_speed) # set speed of game
+        clock.tick(config.SNAKE_SPEED) # set speed of game
 
     pygame.quit()
     quit()
